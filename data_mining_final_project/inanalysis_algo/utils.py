0c8ea0581cea452edddeb8be94cb7e64169a9b0d
Sreyansh Jain <sreyanshjain05@gmail.com>
2018-09-01 18:50:52 +0530

    new algorithms added

--- data_mining_final_project/inanalysis_algo/utils.py
+++ data_mining_final_project/inanalysis_algo/utils.py
@@ -1,10 +1,14 @@
 import logging
 from inanalysis_algo.classification.knn import Knn
 from inanalysis_algo.classification.dc_tree import DCtree
+from inanalysis_algo.classification.rf import RFC
 from inanalysis_algo.abnormal_detection.one_class_svm import OneClassSVM
+from inanalysis_algo.abnormal_detection.isolation_forest import IsolationForest
 from inanalysis_algo.regression.linear_regression import LinearRegression
+from inanalysis_algo.regression.bayesian_regression import BayesianRegression
 from inanalysis_algo.regression.polynomial_regression import PolynomialRegression
 from inanalysis_algo.clustering.kmeans import Kmeans
+from inanalysis_algo.clustering.meanshift import MeanShift
 from inanalysis_algo.clustering.affinitypropagation import AffinityPropagation
 import enum
 logging.basicConfig(level=logging.DEBUG)
@@ -16,6 +20,10 @@ class Algorithm(enum.Enum):
         "algo_name": "one-class_SVM",
         "project_type": "abnormal-detection"
     }
+    isolation_forest = {
+        "algo_name": "isolation-forest",
+        "project_type": "abnormal-detection"
+    }
     knn = {
         "algo_name": "knn",
         "project_type": "classification"
@@ -24,8 +32,16 @@ class Algorithm(enum.Enum):
         "algo_name": "decision-tree",
         "project_type": "classification"
     }
+    rf = {
+        "algo_name": "random-forest",
+        "project_type": "classification"
+    }
     linear_regression = {
         "algo_name": "linear-regression",
+        "project_type": "regression"
+    }
+    bayesian_regression = {
+        "algo_name": "bayesian-regression",
         "project_type": "regression"
     }
     polynomial_regression = {
@@ -34,6 +50,10 @@ class Algorithm(enum.Enum):
     }
     k_means = {
         "algo_name": "k-means",
+        "project_type": "clustering"
+    }
+    meanshift = {
+        "algo_name": "mean-shift",
         "project_type": "clustering"
     }
     affinity_propagation = {
@@ -54,21 +74,33 @@ class AlgoUtils:
         if model_method == Algorithm.one_class_svm.value['algo_name']:
             log.debug("Abnormal-detection one-class_SVM Training")
             algo = OneClassSVM()
+        elif model_method == Algorithm.isolation_forest.value['algo_name']:
+            log.debug("Abnormal-detection isolation-forest Training")
+            algo = IsolationForest()
         elif model_method == Algorithm.knn.value['algo_name']:
             log.debug("Classification knn Training")
             algo = Knn()
         elif model_method == Algorithm.dc_tree.value['algo_name']:
             log.debug("Classification dc-tree Training")
             algo = DCtree()
+        elif model_method == Algorithm.rf.value['algo_name']:
+            log.debug("Classification random-forest Training")
+            algo = RFC()
         elif model_method == Algorithm.linear_regression.value['algo_name']:
             log.debug("Regression linear-regression Training")
             algo = LinearRegression()
+        elif model_method == Algorithm.bayesian_regression.value['algo_name']:
+            log.debug("Regression bayesian-regression Training")
+            algo = BayesianRegression()
         elif model_method == Algorithm.polynomial_regression.value['algo_name']:
             log.debug("Regression polynomial_regression Training")
             algo = PolynomialRegression()
         elif model_method == Algorithm.k_means.value['algo_name']:
             log.debug("Clustering k-means Training")
             algo = Kmeans()
+        elif model_method == Algorithm.meanshift.value['algo_name']:
+            log.debug("Clustering mean-shift Training")
+            algo = MeanShift()
         elif model_method == Algorithm.affinity_propagation.value['algo_name']:
             log.debug("Clustering affinity propagation Training")
             algo = AffinityPropagation()
